b5b4edde6b3a58076172b2d9a77a94d854122312
dotmantosh <dotuntosh@gmail.com>
2024-07-28 15:30:46 +0100

    prod redy

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -12,16 +12,17 @@ import { UserService } from '@/lib/services/user.service'
 
 const apiKey = process.env.NEYNAR_API_KEY as string;
 const HOSTNAME = process.env.HOSTNAME || "https://doiq-farcaster-frames.vercel.app"
+// const HOSTNAME = "http://localhost:3000/api"
 
 const app = new Frog({
   assetsPath: '/',
   basePath: '/api',
-  hub: neynarHub({ apiKey: 'NEYNAR_FROG_FM' })
+  hub: neynarHub({ apiKey })
   // hub: neynar({ apiKey })
 } as FrogConstructorParameters)
   .use(neynar(
     {
-      apiKey: 'NEYNAR_FROG_FM',
+      apiKey,
       features: ['interactor', 'cast'],
     }
   ))
@@ -80,74 +81,232 @@ app.frame('/', (c) => {
     ),
     intents: [
       <Button>Start</Button>,
-      <Button.Link href='http://localhost:3000/leaderboard'>Leaderboard</Button.Link>,
+      <Button.Link href={`${HOSTNAME}/leaderboard`}>Leaderboard</Button.Link>,
       <Button.Link href='https://docs.doiq.xyz/'>Learn</Button.Link>,
     ],
   })
 })
 
 app.frame('/doiq', async (c) => {
-  const { buttonValue, status } = c
-  const fruit = buttonValue
+  const { buttonValue, status } = c;
+  const fruit = buttonValue;
 
-  let user = null
-  let isUpdatedMoreThan10Mins = false
-  let lastUpdated = null
-  const tenMinutesAgo = moment().subtract(10, 'minutes')
+  let user = null;
+  let isUpdatedMoreThan10Mins = false;
+  let lastUpdated = null;
+  const tenMinutesAgo = moment().subtract(10, 'minutes');
+
   try {
-    user = (await UserService.FetchUserByFid(c.var.interactor?.fid?.toString() as string)).data
-    lastUpdated = moment(user.updatedAt)
-    isUpdatedMoreThan10Mins = lastUpdated.isBefore(tenMinutesAgo)
-  } catch (error) {
-    console.log(error)
+
+    const response = await UserService.FetchUserByFid(c.var.interactor?.fid?.toString() as string);
+    user = response.data;
+    lastUpdated = moment(user.updatedAt);
+    isUpdatedMoreThan10Mins = lastUpdated.isBefore(tenMinutesAgo);
+    // console.log('User found', user);
+    // console.log('Last Updated:', lastUpdated);
+    // console.log('Is Updated More Than 10 Mins:', isUpdatedMoreThan10Mins);
+    if (isUpdatedMoreThan10Mins) {
+      return c.res({
+        action: `/result`,
+        image: (
+          <div
+            style={{
+              alignItems: 'center',
+              background: 'white',
+              backgroundSize: '100% 100%',
+              display: 'flex',
+              flexDirection: 'column',
+              flexWrap: 'nowrap',
+              height: '100%',
+              justifyContent: 'center',
+              textAlign: 'center',
+              width: '100%',
+            }}
+          >
+            <div
+              style={{
+                color: 'black',
+                fontSize: 90,
+                fontStyle: 'normal',
+                letterSpacing: '-0.025em',
+                lineHeight: 1.4,
+                marginTop: 30,
+                padding: '0 120px',
+                whiteSpace: 'pre-wrap',
+              }}
+            >
+              I like to _________
+            </div>
+          </div>
+        ),
+        intents: [
+          <Button value="doiq">doiq</Button>,
+          <Button value="doiq?">doiq?</Button>,
+          <Button value="doiq!">doiq!</Button>,
+        ],
+      });
+    } else {
+      const minutesLeft = (10 - moment().diff(lastUpdated, 'minutes')).toString();
+      // console.log(minutesLeft)
+      return c.res({
+        action: '/',
+        image: (
+          <div
+            style={{
+              alignItems: 'center',
+              background: 'white',
+              backgroundSize: '100% 100%',
+              display: 'flex',
+              flexDirection: 'column',
+              flexWrap: 'nowrap',
+              height: '100%',
+              justifyContent: 'center',
+              textAlign: 'center',
+              width: '100%',
+            }}
+          >
+            <div
+              style={{
+                color: 'black',
+                fontSize: 62,
+                fontStyle: 'normal',
+                letterSpacing: '-0.025em',
+                lineHeight: 1,
+                marginTop: 30,
+                padding: '0 120px',
+                whiteSpace: 'pre-wrap',
+              }}
+            >
+              {`Hi ${c.var.interactor?.username}, You've doiqed too hard.`}
+            </div>
+            <div
+              style={{
+                color: 'black',
+                fontSize: 40,
+                fontStyle: 'normal',
+                letterSpacing: '-0.025em',
+                lineHeight: 1,
+                marginTop: 30,
+                padding: '0 120px',
+                whiteSpace: 'pre-wrap',
+              }}
+            >
+              {`You can doiq again in about ${minutesLeft} minutes`}
+            </div>
+          </div>
+        ),
+        intents: [
+          <Button>Home</Button>,
+          <Button.Link href={`${HOSTNAME}/leaderboard`}>Leaderboard</Button.Link>,
+        ],
+      });
+    }
+
+  } catch (error: any) {
+    if (error.response && error.response.status === 404) {
+      return c.res({
+        action: `/result`,
+        image: (
+          <div
+            style={{
+              alignItems: 'center',
+              background: 'white',
+              backgroundSize: '100% 100%',
+              display: 'flex',
+              flexDirection: 'column',
+              flexWrap: 'nowrap',
+              height: '100%',
+              justifyContent: 'center',
+              textAlign: 'center',
+              width: '100%',
+            }}
+          >
+            <div
+              style={{
+                color: 'black',
+                fontSize: 90,
+                fontStyle: 'normal',
+                letterSpacing: '-0.025em',
+                lineHeight: 1.4,
+                marginTop: 30,
+                padding: '0 120px',
+                whiteSpace: 'pre-wrap',
+              }}
+            >
+              I like to _________
+            </div>
+          </div>
+        ),
+        intents: [
+          <Button value="doiq">doiq</Button>,
+          <Button value="doiq?">doiq?</Button>,
+          <Button value="doiq!">doiq!</Button>,
+        ],
+      });
+    } else {
+      // console.log('Error:', error.message);
+      return c.res({
+        image: (
+          <div
+            style={{
+              alignItems: 'center',
+              background: 'white',
+              backgroundSize: '100% 100%',
+              display: 'flex',
+              flexDirection: 'column',
+              flexWrap: 'nowrap',
+              height: '100%',
+              justifyContent: 'center',
+              textAlign: 'center',
+              width: '100%',
+            }}
+          >
+            <div
+              style={{
+                color: 'black',
+                fontSize: 62,
+                fontStyle: 'normal',
+                letterSpacing: '-0.025em',
+                lineHeight: 1,
+                marginTop: 30,
+                padding: '0 120px',
+                whiteSpace: 'pre-wrap',
+              }}
+            >
+              An error occurred: {error.message}
+            </div>
+          </div>
+        ),
+      });
+    }
   }
 
 
+});
 
-  if (!user || isUpdatedMoreThan10Mins) {
+
+app.frame('/result', async (c) => {
+  const { buttonValue, status } = c
+
+  try {
+    let user = (await UserService.FetchUserByFid(c.var.interactor?.fid?.toString() as string)).data
+    // console.log('user found: ', user)
+    const userData = {
+      doiqValue: buttonValue
+    }
+    user = (await UserService.UpdateUser(user.fid, userData)).data
+    // console.log("user updated")
+    const lastUpdated = moment(user.updatedAt)
+    const tenMinutesAgo = moment().subtract(10, 'minutes')
+    let nextDoiqTime = "NOW"
+
+    if (lastUpdated.isAfter(tenMinutesAgo)) {
+      const minutesLeft = (10 - moment().diff(lastUpdated, 'minutes')).toString()
+      nextDoiqTime = `${minutesLeft} minutes`
+    }
+    // console.log("lastUpdated: ", lastUpdated)
+    // console.log("next doiq time: ", nextDoiqTime)
     return c.res({
-      action: `/result`,
-      image: (
-        <div
-          style={{
-            alignItems: 'center',
-            background: 'white',
-            backgroundSize: '100% 100%',
-            display: 'flex',
-            flexDirection: 'column',
-            flexWrap: 'nowrap',
-            height: '100%',
-            justifyContent: 'center',
-            textAlign: 'center',
-            width: '100%',
-          }}
-        >
-          <div
-            style={{
-              color: 'black',
-              fontSize: 90,
-              fontStyle: 'normal',
-              letterSpacing: '-0.025em',
-              lineHeight: 1.4,
-              marginTop: 30,
-              padding: '0 120px',
-              whiteSpace: 'pre-wrap',
-            }}
-          >
-            I like to _________
-          </div>
-        </div>
-      ),
-      intents: [
-        <Button value="doiq">doiq</Button>,
-        <Button value="doiq?">doiq?</Button>,
-        <Button value="doiq!">doiq!</Button>,
-      ],
-    })
-  } else {
-    const minutesLeft = 10 - moment().diff(lastUpdated, 'minutes')
-    return c.res({
-      action: '/',
       image: (
         <div
           style={{
@@ -175,7 +334,7 @@ app.frame('/doiq', async (c) => {
               whiteSpace: 'pre-wrap',
             }}
           >
-            Ops... You've doiqed too hard.
+            {`Good choice ${c.var.interactor?.username}, Your answer has been received by the great doiq himself.`}
           </div>
           <div
             style={{
@@ -189,161 +348,129 @@ app.frame('/doiq', async (c) => {
               whiteSpace: 'pre-wrap',
             }}
           >
-            You can doiq again in about {minutesLeft} minutes
+            {`You can doiq again in about ${nextDoiqTime} minutes`}
           </div>
         </div>
       ),
       intents: [
-        <Button>Home</Button>,
-        <Button.Link href={`${HOSTNAME}/leaderboard`}>Leaderboard</Button.Link>,
+        <Button.Reset>Home</Button.Reset>,
+        <Button.Link href={`${HOSTNAME}/leadearboard`}>Leaderboard</Button.Link>,
       ],
     })
-  }
 
-  // console.log("c: ", c)
-})
+  } catch (error: any) {
+    // console.log('error : ', error)
+    if (error.response && error.response.status === 404) {
+      const userData = {
+        username: c.var.interactor?.username,
+        displayName: c.var.interactor?.displayName,
+        fid: c.var.interactor?.fid?.toString() as string,
+        doiqValue: c.buttonValue,
+      }
+      const user = (await UserService.CreateUser(userData)).data
+      const lastUpdated = moment(user.updatedAt)
+      const tenMinutesAgo = moment().subtract(10, 'minutes')
+      let nextDoiqTime = "NOW"
 
-app.frame('/result', async (c) => {
-  const { buttonValue, status } = c
-  let user
-  try {
-    user = (await UserService.FetchUserByFid(c.var.interactor?.fid?.toString() as string)).data
-  } catch (error) {
-    console.log(error)
-  }
+      if (lastUpdated.isAfter(tenMinutesAgo)) {
+        const minutesLeft = (10 - moment().diff(lastUpdated, 'minutes')).toString()
+        nextDoiqTime = `${minutesLeft} minutes`
+      }
+      // console.log("lastUpdated: ", lastUpdated)
+      // console.log("next doiq time: ", nextDoiqTime)
+      return c.res({
+        image: (
+          <div
+            style={{
+              alignItems: 'center',
+              background: 'white',
+              backgroundSize: '100% 100%',
+              display: 'flex',
+              flexDirection: 'column',
+              flexWrap: 'nowrap',
+              height: '100%',
+              justifyContent: 'center',
+              textAlign: 'center',
+              width: '100%',
+            }}
+          >
+            <div
+              style={{
+                color: 'black',
+                fontSize: 62,
+                fontStyle: 'normal',
+                letterSpacing: '-0.025em',
+                lineHeight: 1,
+                marginTop: 30,
+                padding: '0 120px',
+                whiteSpace: 'pre-wrap',
+              }}
+            >
+              {`Good choice ${c.var.interactor?.username}, Your answer has been received by the great doiq himself.`}
+            </div>
+            <div
+              style={{
+                color: 'black',
+                fontSize: 40,
+                fontStyle: 'normal',
+                letterSpacing: '-0.025em',
+                lineHeight: 1,
+                marginTop: 30,
+                padding: '0 120px',
+                whiteSpace: 'pre-wrap',
+              }}
+            >
+              {`You can doiq again in about ${nextDoiqTime}`}
+            </div>
+          </div>
+        ),
+        intents: [
+          <Button.Reset>Home</Button.Reset>,
+          <Button.Link href={`${HOSTNAME}/leadearboard`}>Leaderboard</Button.Link>,
+        ],
+      })
 
-  if (!user) {
-    const userData = {
-      username: c.var.interactor?.username,
-      displayName: c.var.interactor?.displayName,
-      fid: c.var.interactor?.fid.toString() as string,
-      doiqValue: c.buttonValue,
+      // console.log('user created: ', user)
+    } else {
+      console.log('Error:', error.message);
+      return c.res({
+        image: (
+          <div
+            style={{
+              alignItems: 'center',
+              background: 'white',
+              backgroundSize: '100% 100%',
+              display: 'flex',
+              flexDirection: 'column',
+              flexWrap: 'nowrap',
+              height: '100%',
+              justifyContent: 'center',
+              textAlign: 'center',
+              width: '100%',
+            }}
+          >
+            <div
+              style={{
+                color: 'black',
+                fontSize: 62,
+                fontStyle: 'normal',
+                letterSpacing: '-0.025em',
+                lineHeight: 1,
+                marginTop: 30,
+                padding: '0 120px',
+                whiteSpace: 'pre-wrap',
+              }}
+            >
+              An error occurred: {error.message}
+            </div>
+          </div>
+        ),
+      });
     }
-
-    user = (await UserService.CreateUser(userData)).data
-  } else {
-    const userData = {
-      doiqValue: buttonValue
-    }
-    user = (await UserService.UpdateUser(c.var.interactor?.fid?.toString() as string, userData)).data
   }
 
-
-  const lastUpdated = moment(user.updatedAt)
-  const tenMinutesAgo = moment().subtract(10, 'minutes')
-  let nextDoiqTime = "NOW"
-
-  if (lastUpdated.isAfter(tenMinutesAgo)) {
-    const minutesLeft = 10 - moment().diff(lastUpdated, 'minutes')
-    nextDoiqTime = `${minutesLeft} minutes`
-  }
-
-  return c.res({
-    image: (
-      <div
-        style={{
-          alignItems: 'center',
-          background: 'white',
-          backgroundSize: '100% 100%',
-          display: 'flex',
-          flexDirection: 'column',
-          flexWrap: 'nowrap',
-          height: '100%',
-          justifyContent: 'center',
-          textAlign: 'center',
-          width: '100%',
-        }}
-      >
-        <div
-          style={{
-            color: 'black',
-            fontSize: 62,
-            fontStyle: 'normal',
-            letterSpacing: '-0.025em',
-            lineHeight: 1,
-            marginTop: 30,
-            padding: '0 120px',
-            whiteSpace: 'pre-wrap',
-          }}
-        >
-          Your answer has been received by the great doiq himself.
-        </div>
-        <div
-          style={{
-            color: 'black',
-            fontSize: 40,
-            fontStyle: 'normal',
-            letterSpacing: '-0.025em',
-            lineHeight: 1,
-            marginTop: 30,
-            padding: '0 120px',
-            whiteSpace: 'pre-wrap',
-          }}
-        >
-          You can doiq again in about {nextDoiqTime}
-        </div>
-      </div>
-    ),
-    intents: [
-      <Button.Reset>Home</Button.Reset>,
-      <Button.Link href='http://localhost:3000/leadearboard'>Leaderboard</Button.Link>,
-    ],
-  })
 })
-app.frame('/forbidden', (c) => {
-  const { buttonValue, status } = c
-  return c.res({
-    image: (
-      <div
-        style={{
-          alignItems: 'center',
-          background: 'white',
-          backgroundSize: '100% 100%',
-          display: 'flex',
-          flexDirection: 'column',
-          flexWrap: 'nowrap',
-          height: '100%',
-          justifyContent: 'center',
-          textAlign: 'center',
-          width: '100%',
-        }}
-      >
-        <div
-          style={{
-            color: 'black',
-            fontSize: 62,
-            fontStyle: 'normal',
-            letterSpacing: '-0.025em',
-            lineHeight: 1,
-            marginTop: 30,
-            padding: '0 120px',
-            whiteSpace: 'pre-wrap',
-          }}
-        >
-          Your answer has been recieved by the great doiq himself.
-        </div>
-        <div
-          style={{
-            color: 'black',
-            fontSize: 40,
-            fontStyle: 'normal',
-            letterSpacing: '-0.025em',
-            lineHeight: 1,
-            marginTop: 30,
-            padding: '0 120px',
-            whiteSpace: 'pre-wrap',
-          }}
-        >
-          You can doiq again in about X minutes
-        </div>
-      </div>
-    ),
-    intents: [
-      <Button.Reset>Home</Button.Reset>,
-      <Button.Link href='http://localhost:3000/leaderboard'>Leaderboard</Button.Link>,
-    ],
-  })
-})
+
 
 devtools(app, { serveStatic })
 
