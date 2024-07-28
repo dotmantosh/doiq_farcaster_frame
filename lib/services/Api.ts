e8b3225f70cd427a0f31c690c95b8d95a022df1c
dotmantosh <dotuntosh@gmail.com>
2024-07-28 15:35:01 +0100

    base url changed to live

--- lib/services/Api.ts
+++ lib/services/Api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { ApiRoutes } from "./apiRoutes";
 
-const baseURL = ApiRoutes.BASE_URL_DEV
-// const baseURL = ApiRoutes.BASE_URL_LIVE
+// const baseURL = ApiRoutes.BASE_URL_DEV
+const baseURL = ApiRoutes.BASE_URL_LIVE
 const Api = () => {
   return axios.create({
     baseURL,
