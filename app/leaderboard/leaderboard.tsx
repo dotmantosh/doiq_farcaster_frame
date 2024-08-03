c51dd061e09a9d2f32cb78ddf518d821bd22ebd6
dotmantosh <dotuntosh@gmail.com>
2024-08-03 14:10:18 +0100

    debugings

--- app/leaderboard/leaderboard.tsx
+++ app/leaderboard/leaderboard.tsx
@@ -28,52 +28,55 @@ function Leaderboard() {
     handleFetchLeaderboard()
   }, [])
   return (
-    <div className={styles.leaderboard}>
-      <h1 className="my-2">doiq</h1>
-      <p className="mb-2">The doiqers have doiqed {allDoiqCount} times in total</p>
-      <p className="mb-2">doiq on warpcast</p>
+    // <div className={styles.leaderboard}>
+    //   <h1 className="my-2">doiq</h1>
+    //   <p className="mb-2">The doiqers have doiqed {allDoiqCount} times in total</p>
+    //   <p className="mb-2">doiq on warpcast</p>
 
-      <h2 className="mb-2">Leaderboard Updates in real time</h2>
+    //   <h2 className="mb-2">Leaderboard Updates in real time</h2>
 
 
-      <table>
-        <thead>
-          <tr>
-            <th>Rank</th>
-            <th>Username</th>
-            <th>Successful doiqs</th>
-            <th>Unsuccessful doiqs</th>
-          </tr>
-        </thead>
-        {
-          isFetchingLeaderboard ?
-            <div className="d-flex justify-content-center">
-              <Spinner size="32">Loading...</Spinner>
-            </div>
+    //   <table>
+    //     <thead>
+    //       <tr>
+    //         <th>Rank</th>
+    //         <th>Username</th>
+    //         <th>Successful doiqs</th>
+    //         <th>Unsuccessful doiqs</th>
+    //       </tr>
+    //     </thead>
+    //     {
+    //       isFetchingLeaderboard ?
+    //         <div className="d-flex justify-content-center">
+    //           <Spinner size="32">Loading...</Spinner>
+    //         </div>
 
-            :
-            <tbody>
-              {
-                leaderboard.length > 0 && leaderboard.map((user, index) => {
-                  const doiqCorrectAnswerCount = user.doiqCorrectAnswerCount ?? 0;
-                  const incorrectAnswerCount = (user.doiqCount ?? 0) - doiqCorrectAnswerCount;
+    //         :
+    //         <tbody>
+    //           {
+    //             leaderboard.length > 0 && leaderboard.map((user, index) => {
+    //               const doiqCorrectAnswerCount = user.doiqCorrectAnswerCount ?? 0;
+    //               const incorrectAnswerCount = (user.doiqCount ?? 0) - doiqCorrectAnswerCount;
 
-                  return (
-                    <tr key={index}>
-                      <td>{index + 1}</td>
-                      <td>{user.username}</td>
-                      <td>{doiqCorrectAnswerCount}</td>
-                      <td>{incorrectAnswerCount}</td>
-                    </tr>
-                  );
-                })
-              }
-            </tbody>
+    //               return (
+    //                 <tr key={index}>
+    //                   <td>{index + 1}</td>
+    //                   <td>{user.username}</td>
+    //                   <td>{doiqCorrectAnswerCount}</td>
+    //                   <td>{incorrectAnswerCount}</td>
+    //                 </tr>
+    //               );
+    //             })
+    //           }
+    //         </tbody>
 
-        }
-      </table>
+    //     }
+    //   </table>
 
-    </div>
+    // </div>
+    <>
+      Leaderboard
+    </>
   )
 }
 
